0ad2c8f248b0c07d55ff5f68379a490afa88fb46
Mashu Duek <mashuduek@gmail.com>
2019-02-02 13:32:23 -0500

    add styling to review form

--- src/packages/reviews/star_input.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Star = styled.input.attrs({ type: 'radio' })`
-    checked: ${({ checked }) => checked ? true : false}
-`;
-
-export default Star;
