7bcabf06150bfc5b8afc5b8282a43460ba2d36f1
Mashu Duek <mashuduek@gmail.com>
2019-02-11 21:44:55 -0500

    add single review and remove single star

--- src/stories/reviews/star.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Star from '../../packages/reviews/star';
-
-export default () => (
-    <div>
-        <Star checked={true}></Star>
-        <Star checked={false}></Star>
-    </div>
-);
