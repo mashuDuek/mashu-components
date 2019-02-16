e80bea2bdb683a22bfe30b09f9797f3c77193b99
Mashu Duek <mashuduek@gmail.com>
2019-02-16 17:10:38 -0500

    testing

--- pages/App.jsx
+++ pages/App.jsx
@@ -5,6 +5,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <h1>hi</h1>
                 <Organizer></Organizer>
             </div>
         );
