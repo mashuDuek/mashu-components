4617888131cc9a64f012a2fa71d150dd222e59a4
Mashu Duek <mashuduek@gmail.com>
2019-02-16 17:10:38 -0500

    testing

--- components/assets/javascripts/bundle.js
+++ components/assets/javascripts/bundle.js
@@ -28035,7 +28035,7 @@ function (_Component) {
   _createClass(App, [{
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_organizer__WEBPACK_IMPORTED_MODULE_1__["default"], null));
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "hi"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_organizer__WEBPACK_IMPORTED_MODULE_1__["default"], null));
     }
   }]);
 
